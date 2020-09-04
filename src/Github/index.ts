e57ef7cc42fc072b29e72636100f40716f8ffa25
Charles Tison <ctison@pm.me>
2020-09-04 17:33:12 +0200

    feat(github): add K8S_STAGING_IP on ambassador

--- src/Github/index.ts
+++ src/Github/index.ts
@@ -16,7 +16,14 @@ export class Github extends pulumi.ComponentResource {
   constructor(name: string, args: GithubArgs, opts?: pulumi.ComponentResourceOptions) {
     super('github', name, {}, opts)
 
-    const hasura = new Repository('hasura')
+    const hasura = this.createRepository('hasura')
     hasura.createActionSecret('K8S_STAGING', args.staging.kubeconfig)
+
+    const ambassador = this.createRepository('ambassador')
+    ambassador.createActionSecret('K8S_STAGING_IP', args.staging.k8sIP)
+  }
+
+  createRepository(name: string): Repository {
+    return new Repository(name, null, { parent: this })
   }
 }
